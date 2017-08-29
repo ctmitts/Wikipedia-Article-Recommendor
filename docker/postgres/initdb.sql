9c97ac3bd74d3a652e32a5d58a937f97b480565a
Ubuntu <ubuntu@ip-172-31-44-85.us-west-2.compute.internal>
2017-08-29 02:22:44 +0000

    updated with scripts

--- docker/postgres/initdb.sql
+++ docker/postgres/initdb.sql
@@ -1,3 +1,23 @@
-CREATE TABLE wiki_stuff (
-    id INTEGER
+CREATE DATABASE wikipedia;
+\c wikipedia;
+CREATE TABLE pages (
+    pageid INTEGER,
+    title TEXT,
+    article TEXT
 );
+CREATE TABLE categories (
+    category_id serial PRIMARY KEY,
+    category TEXT
+);
+CREATE TABLE subcategories (
+    subcategory_id serial PRIMARY KEY,
+    subcategory TEXT,
+    category_id INTEGER
+);
+CREATE TABLE page_category (
+    category_page_id serial Primary KEY,
+    pageid INTEGER,
+    subcategory_id INTEGER
+);
+
+
